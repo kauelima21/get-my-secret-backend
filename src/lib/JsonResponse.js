80bc0bcf533b220664cd066e5b26ef1343f8c6c7
kauelima21 <kaueslim@outlook.com>
2022-07-16 23:09:51 -0300

    Allow React app use the API

--- src/lib/JsonResponse.js
+++ src/lib/JsonResponse.js
@@ -2,18 +2,31 @@ export const JsonResponse = {
   _200(data) {
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      },
       body: JSON.stringify(data),
     }
   },
   _201(data) {
     return {
       statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify(data),
     }
   },
   _204(data) {
     return {
       statusCode: 204,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      },
       body: JSON.stringify(data),
     }
   },
